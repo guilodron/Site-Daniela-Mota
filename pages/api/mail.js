fa57a8a834566d917582d4bba12f495ae9c8a5a3
Guilherme Rodrigues <guilodron7@gmail.com>
2020-07-31 19:09:33 -0300

    Remove debug code

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -3,13 +3,6 @@ import nodemailer from 'nodemailer';
 export default async (req, res) => {
   console.log(req.body);
   const {name, emailOrPhone, text} = req.body;
-  // await sendMail({
-  //   name,
-  //   emailOrPhone,
-  //   text
-  // });
-  console.log(name, emailOrPhone, text, process.env.EMAIL_USERNAME, process.env.EMAIL_PASSWORD);
-
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
     service: "Gmail",
@@ -17,9 +10,7 @@ export default async (req, res) => {
       user: process.env.EMAIL_USERNAME, // generated ethereal user
       pass: process.env.EMAIL_PASSWORD, // generated ethereal password
     },
-  });
-
-  
+  });  
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
@@ -30,11 +21,6 @@ export default async (req, res) => {
     html: `<p>Telefone ou email para contato: ${emailOrPhone}<br> Mensagem: ${text}<p>`, // html body
   });
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   res.statusCode = 200
   res.json({ name: 'John Doe' })
 }
