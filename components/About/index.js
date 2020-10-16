cb1ad72a4ae5f97cefa2bb642323f57ae673b4b7
Guilherme Rodrigues <guilodron7@gmail.com>
2020-10-16 00:59:43 -0300

    Changes in about me

--- components/About/index.js
+++ components/About/index.js
@@ -13,13 +13,13 @@ const About = () => {
           Sobre mim
         </h2>
         <p>
+        Consultoria e treinamento para condomínios, prestadoras de serviços e empresas diante da LGPD (Lei Geral de Proteção de Dados).<br/><br/>
         Advogada especialista em Direito Condominial e Imobiliário.<br/><br/>
         Consultora da Comissão Especial de Direito Condominial da OAB Federal.<br/><br/>
         Pós graduada em Direito Notarial e Registral.<br/><br/>
         Curso de extensão em Direito Imobiliário.<br/><br/>
         Palestrante.<br/><br/>
         Cursos de aperfeiçoamento e atualização para interessados no ramo Condominial e Imobiliário. <br/><br/>
-        Expertise na área de segurança de dados (LGPD) para condomínios, administradoras e escritórios de advocacia.<br/><br/>
         </p>
       </div>
     </section>
